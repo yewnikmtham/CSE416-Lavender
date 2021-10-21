31e2c065740f05206f8415284583575928ab4b7f
Michelle Tham <michelle.tham@stonybrook.edu>
2021-10-20 20:57:58 -0400

    Started sidebar

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+function Sidebar() {
+    const classes = useStyles();
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Sidebar
